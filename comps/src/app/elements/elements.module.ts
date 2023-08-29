d72449ffdb4e2db5f93af54b03b76bdf03ba6d46
Razvan-Bogdan Turiatca <rturiatca@griddynamics.com>
2023-08-29 11:33:11 +0300

    implemented routing and created collections and elements routes

---
+++ comps/src/app/elements/elements.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { ElementsRoutingModule } from './elements-routing.module';
+// import { ElementsComponent } from './elements.component';
+import { ElementsHomeComponent } from './elements-home/elements-home.component';
+
+@NgModule({
+  declarations: [ElementsHomeComponent],
+  imports: [CommonModule, ElementsRoutingModule],
+  exports: [],
+})
+export class ElementsModule {}
--- pages/src/app/app.module.ts
+++ pages/src/app/app.module.ts
@@ -4,12 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { ClassDirective } from './class.directive';
 import { TimesDirective } from './times.directive';
+import { CollectionsHomeComponent } from './collections/collections-home/collections-home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ClassDirective,
-    TimesDirective
+    TimesDirective,
+    CollectionsHomeComponent
   ],
   imports: [
     BrowserModule
